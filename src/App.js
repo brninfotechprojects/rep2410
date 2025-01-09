35ae15677651b94ada72f80857aa9b691d4de323
brninfotechprojects <159017463+brninfotechprojects@users.noreply.github.com>
2025-01-09 11:39:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This heading is from react application.</h1>
+    <h1>This code is added by Manjunadh on behalf of Prashanth and Karthik</h1>
     </div>
   );
 }
